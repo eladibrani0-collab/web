00c9ca0fd1ab7b9791b7cbf1083b5e56e9ac3c68
eladibrani0-collab <eladibrani0@gmail.com>
2026-05-18 19:10:40 +0200

    module28

---
+++ module27/js/main.js
@@ -0,0 +1,82 @@
+function showsms(){
+    alert("ky eshte vetem nje message")
+
+
+}
+
+
+showsms();
+
+
+function sum(nr1,nr3){
+    return nr1 +nr3;
+
+
+}
+console.log(sum(2,4));
+document.write(sum(3,4));
+
+
+function localvariable(){
+    var vl="Digital school";
+    alert(vl);
+}
+
+
+localvariable();
+
+
+var car={
+    name:"Audi",
+    color:"red",
+    year:2000,
+    kilometers:10,
+    starEngine:function(){
+        alert("vroom");
+    },
+    get getkilometers(){
+        return this.kilometers;
+    },
+    set setkilometers(km){
+        this.kilometers=km;
+    }
+
+
+};
+
+
+console.log(car.getkilometers);
+
+
+car.setkilometers=100;
+
+
+console.log(car.getkilometers);
+
+
+alert(car.name);
+alert(car['color']);
+
+
+
+car.starEngine();
+
+
+function Computuer(name,cpu,ram,rom){
+    this.name=name;
+    this.cpu=cpu;
+    this.ram=ram;
+    this.rom=rom;
+}
+
+var com1= new Computer("macbook","8-core","8gb","8gb");
+var com2= new Computer("macbook2","6-core","8gb","8gb");
+
+function Car(name,color,year,modelin){
+    this.name=name;
+    this.color=color;
+    this.year=year;
+    this.modelin=modelin;
+}
+
+var car1= new Car("audi2","black",2026,"q3");
