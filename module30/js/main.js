fc9058fa5bb78637352d91acda6485db74823a49
eladibrani0-collab <eladibrani0@gmail.com>
2026-05-25 19:17:56 +0200

    module30

---
+++ module30/js/main.js
@@ -0,0 +1,21 @@
+var input=document.getElementById('input_id');
+var button=document.getElementById('btn_id');
+var text=document.getElementById('text_id');
+
+
+
+button.onclick=function(){
+    if(input.value >12){
+        text.innerHTML="input eshte me i madh se 12";
+    }
+    else if(input.value<12){
+        text.innerHTML="input eshte me i vogel se 12";
+
+    }
+    else{
+        text.innerHTML="input eshte me i barabarte me 12";
+    }    
+
+
+
+}
